7da08f1fba9778127f0ae77fe2fddc6e9ffd14f4
HyesuChoi <chlgotn0580@naver.com>
2023-05-26 21:30:31 +0900

    scroll offset 정리

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,10 +7,22 @@ const Header = () => {
         <Link to="home" smooth={true} spy={true} activeClass="nav-active">
           <li className="nav-link">HOME</li>
         </Link>
-        <Link to="about" smooth={true} spy={true} activeClass="nav-active">
+        <Link
+          to="about"
+          smooth={true}
+          spy={true}
+          activeClass="nav-active"
+          offset={-80}
+        >
           <li className="nav-link">ABOUT</li>
         </Link>
-        <Link to="works" smooth={true} spy={true} activeClass="nav-active">
+        <Link
+          to="works"
+          smooth={true}
+          spy={true}
+          activeClass="nav-active"
+          offset={-80}
+        >
           <li className="nav-link">MY WORKS</li>
         </Link>
         <Link to="contact" smooth={true} spy={true} activeClass="nav-active">
