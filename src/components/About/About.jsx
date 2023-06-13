3028a54e4b11585ca8e637ad5488850136b41944
HyesuChoi <chlgotn0580@naver.com>
2023-06-13 16:02:41 +0900

    skill 수정

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
+import Html from "../../assets/skills-html.png";
+import Js from "../../assets/skills-js.png";
+import Css from "../../assets/skills-css.png";
+import Jq from "../../assets/skills-jq.png";
+import Jq2 from "../../assets/skills-jq2.png";
+import ReactIcon from "../../assets/skills-react.png";
+import Ts from "../../assets/skills-ts.png";
+
+import Git from "../../assets/skills-git.png";
+import Slack from "../../assets/skills-slack.png";
+import Figma from "../../assets/skills-figma.png";
+
 const About = () => {
   return (
     <div id="about" className="about section">
@@ -31,7 +43,45 @@ const About = () => {
       <div className="about-skill-box">
         <h2>MY SKILLS</h2>
 
-        <div className="skills-wrapper">
+        <div className="frontend-skills">
+          <h1>FRONT-END SKILLS</h1>
+          <div className="skills-wrapper">
+            <div className="skill-card">
+              <img src={Html} alt="" />
+            </div>
+            <div className="skill-card">
+              <img src={Js} alt="" />
+            </div>
+            <div className="skill-card">
+              <img src={Css} alt="" />
+            </div>
+            <div className="skill-card">
+              <img src={Jq2} alt="" />
+            </div>
+            <div className="skill-card">
+              <img src={ReactIcon} alt="" />
+            </div>
+            {/* <div className="skill-card">
+              <img src={Ts} alt="" />
+            </div> */}
+          </div>
+        </div>
+        <div className="frontend-skills tools">
+          <h1>TOOLS</h1>
+          <div className="skills-wrapper">
+            <div className="skill-card">
+              <img src={Git} alt="" />
+            </div>
+            <div className="skill-card">
+              <img src={Slack} alt="" />
+            </div>
+            <div className="skill-card">
+              <img src={Figma} alt="" />
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="skills-wrapper">
           <div className="card">
             <div className="circle">
               <CircularProgressbar value={95} text={`95%`} />
@@ -57,7 +107,7 @@ const About = () => {
 
             <div className="text">Photoshop / Illustrator</div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
